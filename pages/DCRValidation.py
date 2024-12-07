afd23cd26cfcc7923cbea95ecc60956a05cdd13e
Raj Nayan Samant <raj111samant@gmail.com>
2024-12-07 10:07:18 +0000

    another try

--- pages/DCRValidation.py
+++ pages/DCRValidation.py
@@ -23,15 +23,7 @@ def ProcessUserInput(userInput):
 
 def WideSpaceDefault():
     st.set_page_config(layout="wide", page_title="DCRValidation App", page_icon="✅")
-    st.markdown(
-        """
-        <style>
-        ._container_gzau3_1, ._viewerBadge_nim44_23, .st-emotion-cache-h4xjwg, ._profileContainer_gzau3_53 {
-            display: none;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True)
+
 
 WideSpaceDefault()
 
@@ -77,51 +69,15 @@ if (submitButtonPressed or (prevUserInput != userInput)) and (userInput not in [
             resultMarkdownText = ProcessUserInput(userInput)
             st.write(resultMarkdownText + "\n" + f"⏱ Time taken {timer.ElapsedTime():.6f} secs")
 
-ft = """
-<style>
-a:link , a:visited{
-color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
-background-color: transparent;
-text-decoration: none;
-}
 
-a:hover,  a:active {
-color: #0283C3; /* theme's primary color*/
-background-color: transparent;
-text-decoration: underline;
-}
+st.markdown(
+    """
+    <style>
+    ._container_gzau3_1, ._viewerBadge_nim44_23, .st-emotion-cache-h4xjwg, ._profileContainer_gzau3_53 {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
 
-#page-container {
-  position: relative;
-  min-height: 10vh;
-}
-
-footer{
-    visibility:hidden;
-}
-
-.footer {
-position: relative;
-left: 0;
-top:230px;
-bottom: 0;
-width: 100%;
-background-color: transparent;
-color: #808080; /* theme's text color hex code at 50 percent brightness*/
-text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
-}
-</style>
-
-<div id="page-container">
-
-<div class="footer">
-<p style='font-size: 0.875em;'>Made with <a style='display: inline; text-align: left;' href="https://streamlit.io/" target="_blank">Streamlit</a><br 'style= top:3px;'>
-with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/sape94" target="_blank"> by sape94</a></p>
-</div>
-
-</div>
-"""
-st.write(ft, unsafe_allow_html=True)
-
-WideSpaceDefault()
 # NSMP24091468370, NSMP24091468383, NSMP24091468381, 26240523C3284085, SE540240924065, WS08249037680588
